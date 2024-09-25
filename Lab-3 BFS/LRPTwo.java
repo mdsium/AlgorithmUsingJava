0ea5f111cf7863a7f412c92ebd498e1539ac5d20
md sium <55883376+mdsium@users.noreply.github.com>
2024-09-25 23:47:45 +0600

    Solved
    Output:
    S
    A
    B
    C
    D
    E
    F
    G

--- Lab-3 BFS/LRPTwo.java
+++ Lab-3 BFS/LRPTwo.java
@@ -1,5 +1,37 @@
-package Lab-3 BFS;
+import java.util.*;
 
 public class LRPTwo {
-    
+    static char[] c={'A','B','C','D','E','F','G','S'};
+    static int e[]={2,2,2,2,2,2,3,3};
+    static int list[] []={{3,7},{4,7},{5,7},{0,6},{1,6},{2,6},{3,4,5},{0,1,2}};
+    static int [] checked=new int [20];
+    static int [] que=new int [20];
+    static int first=0,last=0;
+    public static void main (String[] args){
+        int i,j,n;
+        enq(7);
+        while (first<last) {
+            n=dq();
+            for(i=0; i<e[n]; i++){
+                if(notChecked(list[n][i])==1)
+                    enq(list[n][i]);
+            }
+        }
+    }
+    static int notChecked(int n){
+        if(checked[n]==1)
+            return 0;
+        return 1;
+    }
+    static void enq(int n){
+        checked[n]=1;
+        que[last]=n;
+        last++;
+    }
+    static int dq(){
+        // printf("%c",que[first]+65);
+        System.err.println(c[que[first]]+"");
+        first++;
+        return que[first-1];
+    }
 }
