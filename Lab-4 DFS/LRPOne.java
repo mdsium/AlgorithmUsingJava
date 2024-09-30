96e35425fe91c8de47ab9cc056ebfc5f3ee946bd
md sium <55883376+mdsium@users.noreply.github.com>
2024-09-30 23:44:01 +0600

    Update LRPOne.java

--- Lab-4 DFS/LRPOne.java
+++ Lab-4 DFS/LRPOne.java
@@ -2,7 +2,7 @@ import java.util.*;
 public class LRPOne {
     static char [] c={'A','B','C','D','E','F','G','S'};
     static int e[]={2,2,2,2,2,2,3,3};
-    static list [] [] ={{3,7},{4,7},{5,7},{0,6},{1,6},{2,6},{3,4,5},{0,1,2}};
+    static int list [] [] ={{3,7},{4,7},{5,7},{0,6},{1,6},{2,6},{3,4,5},{0,1,2}};
 
     static int [] checked=new int[20];
     static int [] stk=new int[20];
