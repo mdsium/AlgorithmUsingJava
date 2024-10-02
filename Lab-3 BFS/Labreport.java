19ca86dae97d547fc3cc084b497e1c24b7a6c7fd
md sium <55883376+mdsium@users.noreply.github.com>
2024-10-02 23:48:31 +0600

    Update Labreport.java

--- Lab-3 BFS/Labreport.java
+++ Lab-3 BFS/Labreport.java
@@ -1,3 +1,15 @@
 public class Labreport {
-    
+    static int [] [] matrix=new int[20][20];
+    //Make 2D array that will contain the graph
+    public static void main (String [] args) {
+        int e=8, n=5; 
+        System.out.println("Output: ");
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                System.out.print(matrix[i][j]+" ");
+            }
+            System.out.println("");
+        }
+
+    }
 }
