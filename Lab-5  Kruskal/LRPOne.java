aa5db81760daa3175b377a0c5364800024d84cd1
md sium <55883376+mdsium@users.noreply.github.com>
2024-10-05 23:40:17 +0600

    Update LRPOne.java

--- Lab-5  Kruskal/LRPOne.java
+++ Lab-5  Kruskal/LRPOne.java
@@ -1,5 +1,40 @@
 public class LRPOne {
     static char[] c={'A','B','C','D','E','F','G','s'};  
     static int e[]={2,2,2,2,2,2,3,3};
-    static int list[][]={{3,7},{4,7},{5,7},{0,6},{1,6},{2,6},{3,4,5},{0,1,2}};  
+    static int list[][]={{3,7},{4,7},{5,7},{0,6},{1,6},{2,6},{3,4,5},{0,1,2}}; 
+    static int[] checked=new int[20];
+    static int[] stk=new int[20]; 
+    static int top=0;
+    public static void main(String[] args) {
+        int i,n,f=0;
+        while(top!=0){
+            n=stk[top-1];
+            for(i=0;i<e[n];i++){
+                f=0;
+            if(notChecked(list[n][i])==1){
+                push(list[n][i]);
+                f=1;
+                break;
+            }
+        }
+        if(f==0)
+        pop();
+        }
+
+    }
+    static int notChecked(int n){
+        if(checked[n]==1)
+        return 0;
+        return 1;
+    }
+    static int pop(){
+        top--;
+        return stk[top];
+    }
+    static void push(int n){
+        checked[n]=1;
+        System.out.print(c[n]+" ");
+        stk[top]=n;
+        top++;
+    }
 }
