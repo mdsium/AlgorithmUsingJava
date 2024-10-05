ef86ab0b5085edc9443ca80c6eafcf578a39c6b5
md sium <55883376+mdsium@users.noreply.github.com>
2024-10-05 23:35:02 +0600

    A standard DFS implementation puts each vertex of the graph into one of two categories:
    adjacency list of graph figure 1.
    on lab report

--- Lab-5  Kruskal/LRPOne.java
+++ Lab-5  Kruskal/LRPOne.java
@@ -1,4 +1,5 @@
-
 public class LRPOne {
-    
+    static char[] c={'A','B','C','D','E','F','G','s'};  
+    static int e[]={2,2,2,2,2,2,3,3};
+    static int list[][]={{3,7},{4,7},{5,7},{0,6},{1,6},{2,6},{3,4,5},{0,1,2}};  
 }
